946150214acc9a1207a5094c284cbf6de09fe7de
Antonin SIMON <antonin.simon@epsi.fr>
2023-04-04 23:22:48 +0200

    open the camera when we arrive on the activity

--- app/src/main/java/com/antonin/atelierdveloppementmobile/CreateAccountActivity.kt
+++ app/src/main/java/com/antonin/atelierdveloppementmobile/CreateAccountActivity.kt
@@ -1,11 +1,24 @@
 package com.antonin.atelierdveloppementmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 
 class CreateAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        val buttonQRCode=findViewById<ImageButton>(R.id.qrCodeButton)
+
+        buttonQRCode.setOnClickListener(View.OnClickListener {
+            val newIntent =  Intent(application, QRCodeScannerActivity::class.java)
+            startActivity(newIntent)
+        })
+
+
     }
 }
