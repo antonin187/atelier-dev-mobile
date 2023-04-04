1d9c2284bfec851046f467a9ccb3bc1d5796abf1
Antonin SIMON <antonin.simon@epsi.fr>
2023-04-04 23:48:16 +0200

    add the inscription page

--- app/src/main/java/com/antonin/atelierdveloppementmobile/CreateAccountActivity.kt
+++ app/src/main/java/com/antonin/atelierdveloppementmobile/CreateAccountActivity.kt
@@ -7,15 +7,21 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 
-class CreateAccountActivity : AppCompatActivity() {
+class CreateAccountActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
         val buttonQRCode=findViewById<ImageButton>(R.id.qrCodeButton)
+        val buttonForm=findViewById<Button>(R.id.formButton)
 
         buttonQRCode.setOnClickListener(View.OnClickListener {
             val newIntent =  Intent(application, QRCodeScannerActivity::class.java)
+            startActivity(newIntent)
+        })
+
+        buttonForm.setOnClickListener(View.OnClickListener {
+            val newIntent =  Intent(application, FormInscriptionActivity::class.java)
             startActivity(newIntent)
         })
 
